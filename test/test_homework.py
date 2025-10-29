26b4b5cae832fa0c3fcd886e412a98871deccefc
Liana Rechkina <lianarechkina@yandex.ru>
2025-10-29 17:17:31 +0300

    add title assert

--- test/test_homework.py
+++ test/test_homework.py
@@ -7,10 +7,12 @@ def test_google_search(driver):
     
     url = 'https://www.google.com/'
     driver.get(url)
+    assert driver.title == 'Google'
     assert driver.current_url == url
 
 
 def test_github_web(driver):
     url = 'https://github.com/'
     driver.get(url)
+    assert 'GitHub' in driver.title
     assert driver.current_url == url
